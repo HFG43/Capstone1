d368b042255f0c867bc8b7f8fd2e16f9b78c818e
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-05-24 00:28:01 -0600

    Add Array for Speakers

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// ------------MobileMenu-----------
+
 const hamburguer = document.querySelector('.hamburguer-menu');
 const indexContainer = document.querySelector('.mobile-menu-container');
 const closeButton = document.getElementById('mobile-close-cross');
@@ -19,3 +21,44 @@ homeButton.addEventListener('click',closeMobileMenu);
 aboutButton.addEventListener('click',closeMobileMenu);
 programButton.addEventListener('click',closeMobileMenu);
 sponsorButton.addEventListener('click',closeMobileMenu);
+
+// -------------Dynamic Speakers--------------
+
+const speakers = [
+  {
+    id: '0',
+    name: ' ',
+    role: '',
+    role-description: ''
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  }
+];
+
+`<div id="Invited-Speaker-5" class="speakers-container">
+<div class="speaker-full-container">
+  <div class="speaker-picture-container">
+    <img class="race-flag" src="./Images/Pictures/Race-flag.png">
+    <img class="speaker-picture" src="./Images/person-icon--icon-search-engine-3.png">
+  </div>
+  <div class="speaker-profile-data">
+    <h3 class="speaker-profile-data-name">Alan White</h3>
+    <p class="speaker-profile-data-role">National Parks Asociation Patron</p>
+    <div class="separator-speaker"></div>
+    <p class="speaker-profile-data-description">White spent his life dedicated to increase the restoration areas worldwide.</p>
+  </div>
+</div>
+</div>`
