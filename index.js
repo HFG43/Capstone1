e9423cae35bfc1835eb6944a98e0642b96892a5d
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-05-24 00:02:28 -0600

    Home Mobile Menu on place

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const hamburguer = document.querySelector('.hamburguer-menu');
 const indexContainer = document.querySelector('.mobile-menu-container');
 const closeButton = document.getElementById('mobile-close-cross');
+const homeButton = document.querySelector('.home');
+const aboutButton = document.querySelector('.about');
+const programButton = document.querySelector('.program');
+const sponsorButton = document.querySelector('.sponsor');
 
+function mobileMenu () {
+  indexContainer.classList.remove('show');
+}
+hamburguer.addEventListener('click',mobileMenu);
 
+function closeMobileMenu () {
+  indexContainer.classList.add('show');
+}
+closeButton.addEventListener('click',closeMobileMenu);
+homeButton.addEventListener('click',closeMobileMenu);
+aboutButton.addEventListener('click',closeMobileMenu);
+programButton.addEventListener('click',closeMobileMenu);
+sponsorButton.addEventListener('click',closeMobileMenu);
