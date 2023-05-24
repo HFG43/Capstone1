3c2c2ec744a77022999bc9212cc234ed66ee5f59
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-05-23 23:30:10 -0600

    Mobile Menu Design

---
+++ index.js
@@ -0,0 +1,5 @@
+const hamburguer = document.querySelector('.hamburguer-menu');
+const indexContainer = document.querySelector('.mobile-menu-container');
+const closeButton = document.getElementById('mobile-close-cross');
+
+
