5607e935188d56f893731c4d2b2f8e9468d5746e
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-05-24 22:39:00 -0600

    Final changes

--- index.js
+++ index.js
@@ -92,3 +92,13 @@ function loadSpeakers(speaker) {
 for (let speaker = 0; speaker < speakers.length; speaker += 1) {
   loadSpeakers(speaker);
 }
+
+document.addEventListener('scroll', function() {
+  const toolbar_header = document.querySelector('.toolbar-header');
+  const scrollCheck = window.scrollY;
+  if(scrollCheck > 10) {
+    toolbar_header.classList.add('hide-bar');
+  } else {
+    toolbar_header.classList.remove('hide-bar');
+  }
+});
